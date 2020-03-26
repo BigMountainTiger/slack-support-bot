82cde20e8cfa06d87be0be4895b12c60b46a0911
Song Li <da_tou_li@yahoo.com>
2020-03-26 17:22:40 -0400

    Added the dependencies

--- slack-support-bot/app.js
+++ slack-support-bot/app.js
@@ -1,8 +1,11 @@
+require('dotenv').config()
+const token = process.env.BOT_TOKEN;
+
 exports.lambdaHandler = async (event, context) => {
   let response = {
     'statusCode': 200,
     'body': JSON.stringify({
-      message: 'hello world',
+        message: 'hello world'
       })
     };
 
