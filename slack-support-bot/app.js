8da3233b78abc2a841be57d09930d4ba578babc3
Song Li <da_tou_li@yahoo.com>
2020-04-01 17:24:57 -0400

    Adding the file attachment capability

--- slack-support-bot/app.js
+++ slack-support-bot/app.js
@@ -5,29 +5,50 @@ const standardResponses = require('./handlers/standard-responses');
 const dialogLauncher = require('./handlers/dialog_launcher');
 const dataCollector = require('./handlers/data_collector');
 
-exports.lambdaHandler = async (event, context) => {
-  console.log('message - im');
-  console.log(event.body);
-  let body = JSON.parse(event.body || '{}');
-  // message.im
-  response = {
-    'statusCode': 200,
-    'body': JSON.stringify({
-        message: body
-    })
-  };
+const eventTypes = {
+  events: 'events',
+  commands: 'commands'
+};
 
-  return response;
+const parseBody = event => {
+  let body = null;
+  try {
 
-  //let body = qs.parse(event.body);
-  let payload = JSON.parse(body.payload || '{}');
-  if (payload.type === 'dialog_submission') {
-    return await dataCollector.collect(payload);
+    body = JSON.parse(event.body);
+    body.eventTypes = eventTypes.events;
+  } catch(err) {
+
+    body = qs.parse(event.body);
+    body.eventTypes = eventTypes.commands;
   }
 
-  let trigger_id = body.trigger_id
-  if (trigger_id) {
-    return await dialogLauncher.launch(trigger_id);
+  return body;
+};
+
+exports.lambdaHandler = async (event, context) => {
+  let body = parseBody(event);
+
+  if (body.eventTypes === eventTypes.events) {
+    if (body.type === 'url_verification') {
+      return {
+        statusCode: 200, body: JSON.stringify(body)
+      };
+    }
+
+    console.log(body);
+    return standardResponses.EMPTY;
+  }
+
+  if (body.eventTypes === eventTypes.commands) {
+    let payload = JSON.parse(body.payload || '{}');
+    if (payload.type === 'dialog_submission') {
+      return await dataCollector.collect(payload);
+    }
+
+    let trigger_id = body.trigger_id
+    if (trigger_id) {
+      return await dialogLauncher.launch(trigger_id);
+    }
   }
 
   return standardResponses.UNSUPPORTED;
